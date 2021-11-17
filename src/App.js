63cd62cbbaf59d7122d25d42f060a331f1e9e8ab
salma-anika <anikaavaa@gmail.com>
2021-11-17 23:36:38 +0600

    create functional class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+import User from './User';
 import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <h3>Hello world</h3>
+      <User/>
     </div>
   );
 }
---
+++ src/User.js
@@ -0,0 +1,6 @@
+function User(){
+    return (
+        <h3>Hello User</h3>
+    );
+}
+export default User;
